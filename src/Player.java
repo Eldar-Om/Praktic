5ccb5d1238cb9ff1f47b1532bb0055dd92981eaa
Eldar-Om <75469534+Eldar-Om@users.noreply.github.com>
2021-03-04 13:30:06 +0300

    Add files via upload

---
+++ src/Player.java
@@ -0,0 +1,6 @@
+public class Player extends Hero {
+
+    public Player(String name, int health, int strength, int dexterity, int xp, int gold) {
+        super(name, health, strength, dexterity, xp, gold);
+    }
+}
